5c0508d59ee53e119a60c1871571028cd2857964
edwin0258 <edwin0258@gmail.com>
2015-09-11 20:48:39 +0000

    Create exercises successfully

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-   
+  resources :users do
+    resources :exercises
+  end
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
