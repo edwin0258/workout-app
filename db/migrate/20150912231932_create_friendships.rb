41283d67fd46c1547cd65d567ff517bed73df5d1
edwin0258 <edwin0258@gmail.com>
2015-09-13 04:59:50 +0000

    Fix Herkou

--- db/migrate/20150912231932_create_friendships.rb
+++ db/migrate/20150912231932_create_friendships.rb
@@ -1,10 +1,13 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :user, index: true
+      t.references :friend, index: true
 
       t.timestamps null: false
     end
+    add_index :friendships :user
+    add_index :friendships :friend
+    add_index :friendships, [:user, :friend]
   end
 end
